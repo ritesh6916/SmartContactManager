a9f681fdf746fd21b56367af60eb31d8ea284443
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-16 01:43:55 +0530

    Entity for Contact class created.

---
+++ src/main/java/com/rit/smartcontact/templates/Contact.java
@@ -0,0 +1,108 @@
+package com.rit.smartcontact.templates;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "Contacts")
+public class Contact {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(length = 50, nullable = false)
+	private String name;
+	
+	@Column(length = 50, nullable = true,name = "nick_name")
+	private String nickName;
+
+	@Column(length = 15, nullable = false)
+	private String phone;
+
+	@Column(length = 50, unique = true)
+	private String email;
+	
+	private String occupation;
+	
+	@Column(length = 2000)
+	private String info;
+	private String imageUrl;
+
+	public Contact() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getOccupation() {
+		return occupation;
+	}
+
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [id=" + id + ", name=" + name + ", nickName=" + nickName + ", phone=" + phone + ", email="
+				+ email + ", occupation=" + occupation + ", info=" + info + ", imageUrl=" + imageUrl + "]";
+	}
+
+}
