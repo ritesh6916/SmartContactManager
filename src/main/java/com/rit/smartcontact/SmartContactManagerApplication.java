906756dd2d882bd66d9c80bec5658131eb72384e
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-15 14:34:20 +0530

    generated

---
+++ src/main/java/com/rit/smartcontact/SmartContactManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rit.smartcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContactManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContactManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rit/smartcontact/SmartContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rit.smartcontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContactManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
