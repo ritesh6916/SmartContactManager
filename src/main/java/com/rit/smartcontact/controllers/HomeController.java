54b8964c0ef534d71a02ec11d2a334353839a7de
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-18 10:58:09 +0530

    title accepted from controller..

--- src/main/java/com/rit/smartcontact/controllers/HomeController.java
+++ src/main/java/com/rit/smartcontact/controllers/HomeController.java
@@ -1,13 +1,15 @@
 package com.rit.smartcontact.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/home")
-	public String showHome() {
+	public String showHome(Model model) {
+		model.addAttribute("title", "Home - Smart Contact Manager");
 		return "home";
 	}
 }
