2ed76128d36ef3c9c4e6d72adde6fa630e81122e
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-04-03 13:58:24 +0530

    User_dashbord setup with custom base page for user.

--- src/main/java/com/rit/smartcontact/controllers/UserController.java
+++ src/main/java/com/rit/smartcontact/controllers/UserController.java
@@ -1,10 +1,17 @@
 package com.rit.smartcontact.controllers;
 
+import java.security.Principal;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rit.smartcontact.persistence.UserRepository;
+import com.rit.smartcontact.templates.User;
 
 @Controller
 @RequestMapping("/user")
@@ -12,9 +19,18 @@ public class UserController {
 
 	Logger logger = LoggerFactory.getLogger(getClass().getName());
 
-	@GetMapping("/index")
-	public String showDashboard() {
+	@Autowired
+	UserRepository userRepository;
 
+	@GetMapping("/index")
+	public String showDashboard(Model m, Principal principal) {
+
+		String emial = principal.getName();
+		System.out.println(emial);
+
+		User u = userRepository.getUserByUserName(emial);
+		System.out.println(u);
+		m.addAttribute("user", u);
 		return "user/user_dashboard";
 	}
 
