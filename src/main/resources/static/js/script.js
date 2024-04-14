f9f36293e2b1e1b9134f2032050b7f8e6840ce38
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-04-15 00:29:59 +0530

    Sidebar created & CSS added for responsiveness.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,11 @@
 /*alert("The JS is working..")*/
+
+const toggleSidebar = () => {
+	if ($(".sidebar").is(":visible")) {
+		$(".sidebar").css("display", "none");
+		$(".content").css("margin-left", "0%");
+	} else {
+		$(".sidebar").css("display", "block");
+		$(".content").css("margin-left", "20%");
+	}
+}
