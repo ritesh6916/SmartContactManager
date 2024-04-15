f13b920e2a0d80ff3fc9e773b3b007fcb34fe3e7
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-04-15 21:49:31 +0530

    GUI Created for Add-Contact and margin adjusted.

--- src/main/java/com/rit/smartcontact/controllers/UserController.java
+++ src/main/java/com/rit/smartcontact/controllers/UserController.java
@@ -8,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rit.smartcontact.persistence.UserRepository;
+import com.rit.smartcontact.templates.Contact;
 import com.rit.smartcontact.templates.User;
 
 @Controller
@@ -22,16 +25,36 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 
-	@GetMapping("/index")
-	public String showDashboard(Model m, Principal principal) {
-
+	// Adding common data to every response automatically
+	@ModelAttribute
+	public void addCommonData(Model m, Principal principal) {
 		String emial = principal.getName();
 		System.out.println(emial);
 
 		User u = userRepository.getUserByUserName(emial);
 		System.out.println(u);
 		m.addAttribute("user", u);
+	}
+
+	// User Dashboard
+	@GetMapping("/index")
+	public String showDashboard(Model m) {
+
+		m.addAttribute("title", "User Dashboard");
 		return "user/user_dashboard";
 	}
 
+	// SHOW-add-new-contact-Page Handler
+	@GetMapping("/add-contact")
+	public String showAddContactPage(Model m) {
+
+		m.addAttribute("title", "Add Contact");
+		m.addAttribute("contact", new Contact());
+		return "user/add_contact";
+	}
+
+	@PostMapping("/process-add-contact")
+	public String addContact() {
+		return null;
+	}
 }
